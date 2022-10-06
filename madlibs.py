9d69661432888b888b89b3516e68796f73d5964d
n0vay <koshantanu@gmail.com>
2022-10-06 01:57:01 -0400

    ezz

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,11 @@
-youtuber = "Kylie Ying" #str(input("Enter a Youtuber: "))
 
-madlib = f"computer programming is  so  {adj} "
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"computer programming is  so  {adj}! It makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+    
+print(madlib)
